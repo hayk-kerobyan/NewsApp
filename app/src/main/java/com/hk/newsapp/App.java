f0168aae403d54312678065e759925b8d123b71c
Hayk Kerobyan <hayk.kerobyan91@gmail.com>
2018-11-05 15:08:25 +0400

    Leak canary added. Potential memory leaks checked

--- app/src/main/java/com/hk/newsapp/App.java
+++ app/src/main/java/com/hk/newsapp/App.java
@@ -1,11 +1,21 @@
 package com.hk.newsapp;
 
 import com.hk.newsapp.di.DaggerAppComponent;
+import com.squareup.leakcanary.LeakCanary;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
 public class App extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+    }
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
